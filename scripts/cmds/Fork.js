746327ad2e2614603ac5d9b840e513273161bf26
denokipe14-png <denokipe14@gmail.com>
2026-08-31 10:51:31 +0300

    Update Fork.js

--- scripts/cmds/Fork.js
+++ scripts/cmds/Fork.js
@@ -2,7 +2,7 @@ module.exports = {
  config: {
  name: "fork",
  version: "1.6",
- author: "〲MAMUNツ࿐",
+ author: "〲ncs pro",
  countDown: 2,
  role: 0,
  shortDescription: "Official GitHub Fork",
@@ -18,15 +18,15 @@ module.exports = {
  ✦━━━━━━━━━✦
 👑 𝗢𝗙𝗙𝗜𝗖𝗜𝗔𝗟 𝗙𝗢𝗥𝗞 👑
 ✦━━━━━━━━━✦
-👑 𝗢𝗪𝗡𝗘𝗥 ➜ 𝗠𝗔𝗠𝗨𝗡
+👑 𝗢𝗪𝗡𝗘𝗥 ➜ NCS PRO
 🤖 𝗕𝗢𝗧 ➜ 𝗚𝗢𝗔𝗧 𝗕𝗢𝗧 𝗩𝟮
 ━━━━━━━━━━━
 🌐 𝗢𝗙𝗙𝗜𝗖𝗜𝗔𝗟 𝗚𝗜𝗧𝗛𝗨𝗕
-🔗 https://github.com/MAMUN-GOAT-BOT/V2-.git
+🔗 FUCK UR MOTHER 🍑🥒
 ━━━━━━━━━━━
 🎥 𝗩𝗜𝗗𝗘𝗢 𝗧𝗨𝗧𝗢𝗥𝗜𝗔𝗟 📺 https://youtu.be/_Qb7Pq0Xavs?si=-MaOtPPnBPVSbIFX
 ━━━━━━━━━━
-𝗠𝗔𝗠𝗨𝗡 𝗚𝗢𝗔𝗧 𝗕𝗢𝗧 𝗩𝟮
+NCS ANIMATION 𝗚𝗢𝗔𝗧 𝗕𝗢𝗧 𝗩𝟮
 ✦━━━━━━━━━✦
 `
  }
@@ -40,8 +40,8 @@ module.exports = {
  onChat: async function ({ message, event, getLang }) {
  const body = event.body?.trim().toLowerCase();
 
- if (body === "fork") {
- const link = "https://github.com/MAMUN-GOAT-BOT/V2-.git";
+ if (body === "KIPE") {
+ const link = "FUCK YOU BITCH";
  return message.reply(getLang("current", link));
  }
  }
